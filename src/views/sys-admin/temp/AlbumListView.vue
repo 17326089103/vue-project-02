d3711f4583454766223a88edef8b2a12f06591d8
17326089103 <643365704@qq.com>
2023-03-29 17:08:59 +0800

    2023年3月29日17:08:47

--- src/views/sys-admin/temp/AlbumListView.vue
+++ src/views/sys-admin/temp/AlbumListView.vue
@@ -10,23 +10,157 @@
     <el-divider></el-divider>
 
     <el-table :data="tableData" border style="width: 100%">
-      <el-table-column prop="id" label="ID" width="180"></el-table-column>
-      <el-table-column prop="name" label="名称" width="180"></el-table-column>
-      <el-table-column prop="description" label="简介"></el-table-column>
-      <el-table-column prop="sort" label="排序序号" width="180"></el-table-column>
+      <el-table-column prop="id" label="ID" width="60" align="center"></el-table-column>
+      <el-table-column prop="name" label="名称" width="200" header-align="center" show-overflow-tooltip></el-table-column>
+      <el-table-column prop="description" label="简介" header-align="center" show-overflow-tooltip></el-table-column>
+      <el-table-column prop="sort" label="排序序号" width="100" align="center"></el-table-column>
+      <el-table-column label="操作" width="100" align="center">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" circle size="mini"
+                     @click="showEditFormDialog(scope.row)"></el-button>
+          <el-button type="danger" icon="el-icon-delete" circle size="mini"
+                     @click="openDeleteConfirm(scope.row)"></el-button>
+        </template>
+      </el-table-column>
     </el-table>
-  </div>
 
+    <!-- 以下是修改相册的弹出表单Dialog -->
+    <el-dialog title="修改相册" :visible.sync="dialogFormVisible">
+      <el-form :model="editForm">
+        <el-form-item label="名称" :label-width="formLabelWidth">
+          <el-input v-model="editForm.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="简介" :label-width="formLabelWidth">
+          <el-input v-model="editForm.description" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="排序序号" :label-width="formLabelWidth">
+          <el-input v-model="editForm.sort" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitEditForm()">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      tableData: []
+      tableData: [],
+      dialogFormVisible: false,
+      editForm: {
+        id: '',
+        name: '',
+        description: '',
+        sort: ''
+      },
+      formLabelWidth: '120px'
     }
   },
   methods: {
+    showEditFormDialog(album) {
+      // this.editForm = album;
+      // this.dialogFormVisible = true;
+
+      let url = 'http://localhost:9080/albums/' + album.id;
+      console.log('url = ' + url);
+
+      this.axios.get(url).then((response) => {
+        let jsonResult = response.data;
+        if (jsonResult.state == 20000) {
+          this.editForm = jsonResult.data;
+          this.dialogFormVisible = true;
+        } else if (jsonResult.state == 40400) {
+          this.$alert(jsonResult.message, '操作失败', {
+            confirmButtonText: '确定',
+            callback: action => {
+              this.loadAlbumList();
+            }
+          });
+        } else {
+          this.$alert(jsonResult.message, '操作失败', {
+            confirmButtonText: '确定',
+            callback: action => {}
+          });
+        }
+      });
+    },
+    submitEditForm() {
+      let url = 'http://localhost:9080/albums/' + this.editForm.id + '/update';
+      console.log('url = ' + url);
+
+      let formData = this.qs.stringify(this.editForm);
+
+      this.axios.post(url, formData).then((response) => {
+        let jsonResult = response.data;
+        if (jsonResult.state == 20000) {
+          this.$message({
+            message: '修改相册成功！',
+            type: 'success'
+          });
+          this.dialogFormVisible = false;
+          this.loadAlbumList();
+        } else if (jsonResult.state == 40400) {
+          this.$alert(jsonResult.message, '操作失败', {
+            confirmButtonText: '确定',
+            callback: action => {
+              this.dialogFormVisible = false;
+              this.loadAlbumList();
+            }
+          });
+        } else if (jsonResult.state == 40900) {
+          this.$message.error(jsonResult.message);
+        } else {
+          this.$alert(jsonResult.message, '操作失败', {
+            confirmButtonText: '确定',
+            callback: action => {
+              this.dialogFormVisible = false;
+            }
+          });
+        }
+      });
+    },
+    openDeleteConfirm(album) {
+      let message = '此操作将永久删除【' + album.name + '】相册，是否继续？';
+      this.$confirm(message, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.handleDelete(album);
+      }).catch(() => {
+      });
+    },
+    handleDelete(album) {
+      let url = 'http://localhost:9080/albums/' + album.id + '/delete';
+      console.log('url = ' + url);
+
+      this.axios.post(url).then((response) => {
+        let jsonResult = response.data;
+        if (jsonResult.state == 20000) {
+          this.$message({
+            message: '删除【' + album.name + '】相册成功！',
+            type: 'success'
+          });
+          this.loadAlbumList();
+        } else if (jsonResult.state == 40400) {
+          this.$alert(jsonResult.message, '操作失败', {
+            confirmButtonText: '确定',
+            callback: action => {
+              this.loadAlbumList();
+            }
+          });
+        } else {
+          this.$alert(jsonResult.message, '操作失败', {
+            confirmButtonText: '确定',
+            callback: action => {}
+          });
+        }
+      });
+    },
     loadAlbumList() {
       let url = 'http://localhost:9080/albums';
       console.log('url = ' + url);
